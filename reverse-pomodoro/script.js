a5d2292a19db57c8bb505ab4436395b6bd8f9cac
Marcelle Pernet <mevpernet@gmail.com>
2022-07-30 16:16:03 -0300

    today cycles

--- reverse-pomodoro/script.js
+++ reverse-pomodoro/script.js
@@ -6,6 +6,11 @@ const breakWatch = document.querySelector('.breakWatch .time');
 const focusBtn = document.querySelector('#focusbtn');
 const stopFocBtn = document.querySelector('#stopfocusbtn');
 
+const cycle = document.querySelector('#cycle0');
+const focusTime = document.querySelector('#focustime');
+const breakTime = document.querySelector('#breaktime');
+
+
 // Event Listeners
 
 focusBtn.addEventListener('click', startFocus);
@@ -16,6 +21,7 @@ stopFocBtn.addEventListener('click', stopFocus);
 let secs = 0;
 let mins = 0;
 let hrs = 0;
+let num = 1;
 
 function timerFocus() {
     secs++
@@ -58,16 +64,29 @@ function timerBreak() {
 function startFocus() {
     if (secs > 0) {
         clearInterval(breakInterval);
+        cycle.innerText = 'Cycle 0' + num;
+        num++;
+        if (mins < 10) {
+            breakTime.innerText = `Break time: 0${hrs}:0${mins}:${secs}`
+        } else {
+            breakTime.innerText = `Break time: 0${hrs}:${mins}:${secs}`
+        }
     }
     secs = 0;
     mins = 0;
     hrs = 0;
-    breakWatch.innerText = `0${hrs}:0${mins}:${secs}`;
+    breakWatch.innerText = '00:00:00';
     interval = setInterval(timerFocus, 1000);
 }
 
 function stopFocus() {
     clearInterval(interval);
+    cycle.innerText = 'Cycle 0' + num;
+    if (mins < 10) {
+        focusTime.innerText = `Focus time: 0${hrs}:0${mins}:${secs}`
+    } else {
+        focusTime.innerText = `Focus time: 0${hrs}:${mins}:${secs}`
+    }
     secs = 0;
     mins = 0;
     hrs = 0;
