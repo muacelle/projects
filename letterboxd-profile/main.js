4aea3f918f8706e51eb3a623f4abf68b62474c86
Marcelle Pernet <mevpernet@gmail.com>
2022-08-09 15:12:02 -0300

    directors list

--- letterboxd-profile/main.js
+++ letterboxd-profile/main.js
@@ -3,6 +3,7 @@ import fs from 'fs'
 import fetch from 'node-fetch';
 
 const ratingsCsv = fs.readFileSync('ratings.csv', 'utf-8').toString();
+const directorsList = [];
 
 const ratingsObj = parse(ratingsCsv, {
     columns: true,
@@ -19,8 +20,9 @@ async function getMovieId(nameyear) {
 async function getDirector(movieid) {
     const response = await fetch(`https://imdb-api.com/en/API/FullCast/k_sfmpwgoj/${movieid}`);
     const data = await response.json();
-    const director = data.directors.items[0];
-    console.log(director);
+    const director = data.directors.items[0].name;
+    directorsList.push(director);
+    console.log(directorsList);
 }
 
 getMovieId('dogtooth 2009')
