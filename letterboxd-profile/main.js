53d5e3d2499ec684b0864424c6ffe1c918d1a82d
Marcelle Pernet <mevpernet@gmail.com>
2022-08-11 20:00:02 -0300

    data 10-60 100-109

--- letterboxd-profile/main.js
+++ letterboxd-profile/main.js
@@ -2,6 +2,9 @@ import { parse } from 'csv-parse/sync';
 import fs from 'fs'
 import fetch from 'node-fetch';
 
+const keys = ['k_026whftt', 'k_6oy46nx1', 'k_sfmpwgoj', 'k_u8c5djz6'];
+let key = keys[0];
+
 const ratingsCsv = fs.readFileSync('ratings.csv', 'utf-8').toString();
 
 const ratingsObj = parse(ratingsCsv, {
@@ -9,10 +12,10 @@ const ratingsObj = parse(ratingsCsv, {
     skip_empty_lines: true
 });
 
-const last5 = ratingsObj.slice(100, 110);   // os primeiros estão bugados?
+const movielist = ratingsObj.slice(51, 60);   // os primeiros estão bugados? 10-60 100-109
 
 async function getDirectorByMovieName(name, year) {
-    const response = await fetch(`https://imdb-api.com/en/API/SearchMovie/k_64fkc03d/${name} ${year}`);
+    const response = await fetch(`https://imdb-api.com/en/API/SearchMovie/${key}/${name} ${year}`);
     const data = await response.json();
     const movieId = data.results[0].id;
     const result = await getDirector(movieId);
@@ -20,7 +23,7 @@ async function getDirectorByMovieName(name, year) {
 }
 
 async function getDirector(movieid) {
-    let response = await fetch(`https://imdb-api.com/en/API/FullCast/k_64fkc03d/${movieid}`);
+    let response = await fetch(`https://imdb-api.com/en/API/FullCast/${key}/${movieid}`);
     let data = await response.json();
     let director = data.directors.items[0].name;
     return director;
@@ -54,8 +57,9 @@ function getFavorites (list) {
     .sort((a, b) => (a[1] > b[1]) ? -1 : ((b[1] > a[1]) ? 1 : 0)); // organiza por ordem dos + assistidos
 }
 
-/*(async () => {
-    const movies = await directorsRatings(last5);
-    const grouped = getFavorites(movies);
-    console.log(grouped);
-})()*/
+(async () => {
+    const movies = await directorsRatings(movielist);
+    console.log('Saved!');
+})()
+
+//const grouped = getFavorites(movies);
