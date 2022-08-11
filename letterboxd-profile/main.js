b3d2fe35551f35f6de6d9157dec643eeccec0937
Marcelle Pernet <mevpernet@gmail.com>
2022-08-11 19:31:21 -0300

    funcionando!!!

--- letterboxd-profile/main.js
+++ letterboxd-profile/main.js
@@ -9,10 +9,10 @@ const ratingsObj = parse(ratingsCsv, {
     skip_empty_lines: true
 });
 
-const last5 = ratingsObj.slice(-5);
+const last5 = ratingsObj.slice(100, 110);   // os primeiros estão bugados?
 
 async function getDirectorByMovieName(name, year) {
-    const response = await fetch(`https://imdb-api.com/en/API/SearchMovie/k_1p25wqxl/${name} ${year}`);
+    const response = await fetch(`https://imdb-api.com/en/API/SearchMovie/k_64fkc03d/${name} ${year}`);
     const data = await response.json();
     const movieId = data.results[0].id;
     const result = await getDirector(movieId);
@@ -20,7 +20,7 @@ async function getDirectorByMovieName(name, year) {
 }
 
 async function getDirector(movieid) {
-    let response = await fetch(`https://imdb-api.com/en/API/FullCast/k_1p25wqxl/${movieid}`);
+    let response = await fetch(`https://imdb-api.com/en/API/FullCast/k_64fkc03d/${movieid}`);
     let data = await response.json();
     let director = data.directors.items[0].name;
     return director;
@@ -31,10 +31,31 @@ async function directorsRatings(movies) {
         const title = movie.Name;
         const rating = parseFloat(movie.Rating);
         const director = await getDirectorByMovieName(movie.Name, movie.Year);
-        return { title, director, rating };
+        return { title, director, rating };  
     }))
-    console.log(response);
-    return response;
+
+    // save response here
+    let data = fs.readFileSync('data.json');   
+    let completeList = JSON.parse(data);
+    const updatedList = [...completeList, ...response];
+    let jsonstr = JSON.stringify(updatedList, null, 2);
+    fs.writeFileSync('data.json', jsonstr);
+    return updatedList;
 }
 
-directorsRatings(last5);
+function getFavorites (list) {
+    let arr = [];
+    let count = {};
+    list.forEach(elem => { arr.push(elem.director) });             // cria uma arr com todas as entradas de diretores
+    arr.forEach(elem => { count[elem] = (count[elem] || 0) + 1 }); // se o diretor ainda não estiver em arr, count[elem] == false -> count[elem] = 0 + 1
+
+    return Object
+    .entries(count)                                                // transforma em array
+    .sort((a, b) => (a[1] > b[1]) ? -1 : ((b[1] > a[1]) ? 1 : 0)); // organiza por ordem dos + assistidos
+}
+
+/*(async () => {
+    const movies = await directorsRatings(last5);
+    const grouped = getFavorites(movies);
+    console.log(grouped);
+})()*/
