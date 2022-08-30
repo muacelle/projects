5802dd77df6d887bd3c6f1776ba19fb1bc670b11
Marcelle Pernet <mevpernet@gmail.com>
2022-08-30 15:50:35 -0300

    react components Nav n Director

--- letterboxd-profile/App.js
+++ letterboxd-profile/App.js
@@ -1,15 +1,38 @@
-function Directors() {
+function Nav() {
     return (
-        <div>
-            
+        <div className="nav">
+            <h1>Your</h1>
+            <img src="letterboxd-logo.png"/>
+            <h1>Profile</h1>
+        </div>
+    )
+}
+
+function Director(props) {
+    return (
+        <div className="container">
+
+            <div className="card">
+                <h1>{props.ranking}</h1>
+                <img src={props.img}/>
+                <h3>{props.name}</h3>
+                <p>Average Rating: {props.rating}</p>
+            </div>
+
         </div>
     )
 }
 
 function App() {
     return (
-        <div>
-
+        <div class="app">
+            <Nav />
+            <Director 
+            ranking="1"
+            img="https://m.media-amazon.com/images/M/MV5BOGYzMTdiMTAtZjkwZi00NmYwLWE5MTYtNmY1ZmJlMWY4ZGM0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_Ratio0.7273_AL_.jpg"
+            name="Jordan Peele"
+            rating="4.75"
+            />
         </div>
     )
 }
